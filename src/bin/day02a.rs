0b66d992e2cae8140e6c0ed1d24f422dc4f85a6f
Martin Huschenbett <martin.huschenbett@posteo.me>
2021-12-02 10:03:59 +0100

    Solutions for day 2 in Rust

---
+++ src/bin/day02a.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+type Error = Box<dyn std::error::Error>;
+fn main() -> Result<(), Error> {
+    let file = File::open("input/day02.txt")?;
+    let reader = BufReader::new(file);
+    let (mut x_pos, mut y_pos) = (0, 0);
+    for line in reader.lines() {
+        let line = line?;
+        let (cmd, arg) = match &line.trim().split_whitespace().collect::<Vec<&str>>()[..] {
+            [cmd, arg] => (cmd.to_owned(), arg.parse::<i32>()?),
+            _ => panic!("invalid line: {}", line),
+        };
+        match cmd {
+            "forward" => x_pos += arg,
+            "down" => y_pos += arg,
+            "up" => y_pos -= arg,
+            _ => panic!("invalid command: {}", cmd),
+        }
+    }
+    println!("The product of the final position is {}", x_pos * y_pos);
+    Ok(())
+}
